9de03c14b5e65059dbfbf9df6d179a01c5223bc3
vitaliy <vitaliylevitskiand@gmail.com>
2016-04-20 19:36:03 +0300

    add scalling and moving. But it is undone

--- test/game.py
+++ test/game.py
@@ -1,34 +1,44 @@
+import math
 import pygame
+
 import base.engine2D as engine
 
 GREEN = (0, 255, 0)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
+GRAY = (60, 60, 60)
 
 SIZE = (600, 600)
+ZOOM = 20
+D = 20
+B_D = 20
+X = 300
+Y = 300
 
 
 def draw_world(world, screen):
-    ZOOM = 10
-    X = 300
-    Y = 300
-
     def trans(x, y):
         return [int(x * ZOOM) + X, SIZE[1] - (int(y * ZOOM) + Y)]
 
-
     def draw_layout():
-        pygame.draw.line(screen, WHITE, [Y, 0], [Y, SIZE[0]], 2)
-        pygame.draw.line(screen, WHITE, [0, X], [SIZE[1], X], 2)
+        pygame.draw.line(screen, WHITE, [0, SIZE[0] - Y],
+                         [SIZE[0], SIZE[0] - Y], 2)
+        pygame.draw.line(screen, WHITE, [X, 0], [X, SIZE[1]], 2)
+        d = D
+        n = SIZE[0] / d
+        for i in range(0, n / 2):
+            pygame.draw.line(screen, GRAY, [0, SIZE[0] - Y + i * d],
+                             [SIZE[0], SIZE[0] - Y + i * d], 1)
+            pygame.draw.line(screen, GRAY, [X + i * d, 0],
+                             [X + i * d, SIZE[1]], 1)
 
     screen.fill(BLACK)
+    draw_layout()
     for p in world._points:
         pygame.draw.circle(screen, GREEN, trans(p.pos.x, p.pos.y), 3)
     for j in world._joints:
         pygame.draw.line(screen, GREEN, trans(j.p1.pos.x, j.p1.pos.y),
                          trans(j.p2.pos.x, j.p2.pos.y), 1)
-
-    draw_layout()
 
 
 pygame.init()
@@ -41,8 +51,9 @@ crashed = False
 # ini section
 # =============================================================================
 world = engine.World(2)
-for i in range(0, 100):
-    points = [engine.Point2D(0, 10 + i, 1), engine.Point2D(1, 20 + i, 1), engine.Point2D(10, 10 + i, 1)]
+for i in range(0, 100, 10):
+    points = [engine.Point2D(0, 10 + i, 1), engine.Point2D(1, 20 + i, 1),
+              engine.Point2D(10, 10 + i, 1)]
     world.add_points(points)
     world.add_joint(points[0], points[1], 1)
     world.add_joint(points[0], points[2], 1)
@@ -53,6 +64,32 @@ while not crashed:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             crashed = True
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                X += 10
+            if event.key == pygame.K_RIGHT:
+                X -= 10
+            if event.key == pygame.K_UP:
+                Y -= 10
+            if event.key == pygame.K_DOWN:
+                Y += 10
+            if event.key == pygame.K_KP_PLUS:
+                if ZOOM > B_D:
+                    ZOOM += ZOOM / B_D
+                else:
+                    ZOOM += 1
+                D += 1
+                if D == 40:
+                    D = 20
+            if event.key == pygame.K_KP_MINUS:
+                if ZOOM > B_D:
+                    ZOOM -= ZOOM / B_D
+                else:
+                    ZOOM -= 1
+                D -= 1
+                if D == 19:
+                    D = 40
+
     world.step(1.0 / 60)
     draw_world(world, gameDisplay)
     pygame.display.update()
